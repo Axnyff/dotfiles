ffbb9d363403f933cdc7c3592c86158cd725557f
Axel Nyffenegger <Axnyff@gmail.com>
2022-03-03 11:35:28 +0100

    Wip

--- compiler/eslint_formatter.js
+++ compiler/eslint_formatter.js
@@ -1,13 +1,14 @@
-const body = arg => {
-  const [data] = arg;
+const body = (files) => {
   const { messages, filePath } = data;
 
-  return messages
-    .map(
-      ({ line, column, message, source }) =>
-        `${filePath}: line ${line}, col ${column}, ${message}`
-    )
-    .join("\n");
+  return files.flatMap(({ messages, filePath }) =>
+    messages
+      .map(
+        ({ line, column, message, source }) =>
+          `${filePath}: line ${line}, col ${column}, ${message}`
+      )
+      .join("\n")
+  );
 };
 
 module.exports = body;
